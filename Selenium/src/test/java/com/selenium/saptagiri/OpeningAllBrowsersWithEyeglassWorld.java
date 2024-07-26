a899f432a7939b87ae83948b8ac8c9663d4131b2
Sapth <Sapth@Giri>
2024-07-26 19:33:18 +0530

    Opening all browsers with eye glass world url

--- Selenium/src/test/java/com/selenium/saptagiri/OpeningAllBrowsersWithEyeglassWorld.java
+++ Selenium/src/test/java/com/selenium/saptagiri/OpeningAllBrowsersWithEyeglassWorld.java
@@ -1,10 +1,28 @@
 package com.selenium.saptagiri;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class OpeningAllBrowsersWithEyeglassWorld {
-	WebDriver driver = new EdgeDriver();
+	static WebDriver driver;
+
 	public static void main(String[] args) {
-		
-		
+		driver = new EdgeDriver();
+		driver.get("https://www.eyeglassworld.com");
+		driver.manage().window().maximize();
+		driver.quit();
+
+		driver = new ChromeDriver();
+		driver.get("https://www.eyeglassworld.com");
+		driver.manage().window().maximize();
+		driver.quit();
+
+		driver = new FirefoxDriver();
+		driver.get("https://www.eyeglassworld.com");
+		driver.manage().window().maximize();
+		driver.quit();
 
 	}
 
